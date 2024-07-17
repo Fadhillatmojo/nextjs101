0eb792cbaa30b3dca2d58d7d2bb3d51150a55df2
User <fadhillatmojo@gmail.com>
2024-07-17 15:38:29 +0700

    layout dasar dan nested layout

---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,10 @@
+export default function ProductDetailsLayout({ children }: {
+	children: React.ReactNode
+}) {
+	return (
+		<>
+			{children}
+			<h2>Features Products testing layout in product Id level</h2>
+		</>
+	);
+}
