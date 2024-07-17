b5e56f0b94d8fcffab2584b2e98a39c8a908217f
User <fadhillatmojo@gmail.com>
2024-07-17 16:27:23 +0700

    title metadata - absolute, template and default

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	// title: {
+	// 	absolute: "Blog"
+	// }
+	title: "Blog"
+};
 export default function Blog(){
 	return <h1>MY BLOG</h1>;
 }
