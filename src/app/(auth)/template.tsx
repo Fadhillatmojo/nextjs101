466830b2b51d476201261138ff1f40b04e935070
User <fadhillatmojo@gmail.com>
2024-07-17 23:01:40 +0700

    navigating through button form, template.tsx vs layout.tsx, loading UI

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./style.css";
-
+import { useState } from "react";
 
 const navLinks = [
 	{ name: "Register", href: "/register" },
@@ -11,12 +11,17 @@ const navLinks = [
 	{ name: "Forgot Password", href: "/forgot-password" },
 ];
 
+// the different between layout and template is that when the route is change, template.tsx will re render the components, re render the state value, and so on. on the other side, layout.tsx doesnt. 
 export default function AuthLayout({ children }: {
 	children: React.ReactNode;
 }) {
 	const pathName = usePathname();
+	const [input, setInput] = useState("");
 	return (
 		<div>
+			<div>
+				<input type="text" value={input} onChange={e => setInput(e.target.value)} />
+			</div>
 			========== <br />
 			{navLinks.map((link) => {
 				const isActive = pathName.startsWith(link.href);
